da076a49fdd8bdf134037942ae3733aeb55066fd
Alexander Schwartzberg <aeksco@gmail.com>
2018-10-05 10:05:18 -0400

    Text index writes blueprint to file

--- test/index.js
+++ test/index.js
@@ -1,7 +1,10 @@
 const blueprint = require('./blueprint')
+const fs = require('fs')
 
 const { inflateSchema, inflateRelation, inflate } = require('../lib/inflate')
 
 // console.log(blueprint);
 
 const inflatedBlueprint = inflate({ blueprint })
+
+fs.writeFile('inflated.json', JSON.stringify(inflatedBlueprint, null, 2))
